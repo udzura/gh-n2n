e635371b442787024486eb9fbc18f6ba0d24e4cf
Uchio KONDO <udzura@paperboy.co.jp>
2014-05-21 19:54:20 +0900

    First implimentation

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ gem "octokit"
 gem "terminal-notifier"
 gem "dotenv"
 gem "dotenv-configuration"
+
+gem "pry"
--- start-process.rb
+++ start-process.rb
@@ -1,5 +1,27 @@
 require 'bundler'
 Bundler.require
 
-Dotenv.get(
+Dotenv.configure(
+  'GITHUB_ACCESS_TOKEN' => 'visit https://github.com/settings/tokens/new'
 )
+
+Octokit.configure do |c|
+  c.access_token = ENV['GITHUB_ACCESS_TOKEN']
+end
+
+loop do
+  notifications = Octokit.notifications(all: true).select {|i| %w(mention).include? i.reason }
+  last_notification = notifications.first
+  if !@last_update or @last_update < last_notification.updated_at
+    target_pr = last_notification.subject.rels[:self].get.data
+    comments = target_pr.rels[:comments].get.data
+    if comments.empty?
+      p target_pr.body
+    else
+      p comments.last.body
+    end
+    @last_update = last_notification.updated_at
+  end
+  puts "."
+  sleep 10
+end
