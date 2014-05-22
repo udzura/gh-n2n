d636680f67ae79dca8363e821d873c4050287ca1
Uchio KONDO <udzura@paperboy.co.jp>
2014-05-22 10:48:03 +0900

    Show all of notifications found, Access URL

--- start-process.rb
+++ start-process.rb
@@ -10,18 +10,26 @@ Octokit.configure do |c|
 end
 
 loop do
-  notifications = Octokit.notifications(all: true).select {|i| %w(mention).include? i.reason }
+  notifications = Octokit.notifications(all: true, participating: true).select {|i| %w(mention).include? i.reason }
   last_notification = notifications.first
   if !@last_update or @last_update < last_notification.updated_at
-    target_pr = last_notification.subject.rels[:self].get.data
-    comments = target_pr.rels[:comments].get.data
-    if comments.empty?
-      p target_pr.body
-    else
-      p comments.last.body
+    notifications.reverse_each do |notification|
+      target_pr = notification.subject.rels[:self].get.data
+      comments = target_pr.rels[:comments].get.data
+      url = nil
+      if comments.empty?
+        puts target_pr.body.lines.map{|l| l.sub(/\A/, "\t") }.join
+        url = target_pr.html_url
+      else
+        comment = comments.last
+        puts comment.body.lines.map{|l| l.sub(/\A/, "\t") }.join
+        url = comment.html_url
+      end
+      puts "URL: %s" % url
+      puts "-" * 120
     end
     @last_update = last_notification.updated_at
   end
-  puts "."
-  sleep 10
+  puts Time.now
+  sleep 60
 end
